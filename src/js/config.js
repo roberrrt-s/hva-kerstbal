d927e812c734233a0fe11a262c6d217d39b906ba
Robert Spier <robert.spier@gmail.com>
2020-12-09 00:49:17 +0100

    Set time, disable scrolling.

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ import dayjs from 'dayjs';
 
 export const config = {
 	snowflakeAmount: 25,
-	showTime: dayjs('2020-12-7').hour(20).minute(15),
-	buttonTime: dayjs('2020-12-7').hour(20).minute(16)
+	showTime: dayjs('2020-12-7').hour(16).minute(40),
+	buttonTime: dayjs('2020-12-7').hour(15).minute(30)
 }
